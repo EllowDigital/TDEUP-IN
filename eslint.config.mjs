01dc2cd9334cfe1b70e7f50957485f5f5824ea73
Sarwan Yadav <sarwanyadav6174@gmail.com>
2026-06-18 22:11:08 +0530

    FIXED
    fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,19 @@
 import eslintConfigNext from "eslint-config-next";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
-const eslintConfig = [...eslintConfigNext, eslintPluginPrettierRecommended];
+export default [
+  {
+    ignores: [
+      ".next/**",
+      ".open-next/**",
+      "node_modules/**",
+      "dist/**",
+      "out/**",
+      "coverage/**",
+    ],
+  },
 
-export default eslintConfig;
+  ...eslintConfigNext,
+
+  eslintPluginPrettierRecommended,
+];
